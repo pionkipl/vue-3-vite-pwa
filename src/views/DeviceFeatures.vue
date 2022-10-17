e81f0dad63eee94797ac85071e63edce3f2d57e5
Jarek <j.szczepaniak@albedomarketing.pl>
2022-10-17 13:38:05 +0200

    Feat: add battery status.

--- src/views/DeviceFeatures.vue
+++ src/views/DeviceFeatures.vue
@@ -1,16 +1,47 @@
 <template>
   <div class="device-feature">
+    <h2>Battery</h2>
+    <p>Status: {{ batteryCharging ? 'Charging' : 'Not charging' }}</p>
+    <p style="margin-bottom: 1rem">Level: {{ batteryLevel * 100 }}%</p>
     <button @click="vibrate" class="button">Vibrate</button>
   </div>
 </template>
 
 <script setup>
+  import { ref } from 'vue';
+
   function vibrate() {
     navigator.vibrate([100, 200, 100, 200, 100, 200, 100, 200, 100,
       100, 100, 100, 100, 200, 100, 200, 100, 200, 100, 200, 100, 100, 100,
       100, 100, 200, 100, 200, 100, 200, 100, 200, 100, 100, 100, 100, 100,
       100, 100, 100, 100, 100, 50, 50, 100, 800]);
   }
+
+  let batteryCharging = ref(null);
+  let batteryLevel = ref(null);
+
+  let batteryPromise;
+
+  function getBatteryStatus() {
+    if ('getBattery' in navigator) {
+      batteryPromise = navigator.getBattery();
+    } else {
+      batteryPromise = Promise.resolve(navigator.battery);
+    }
+
+    batteryPromise.then(battery => {
+      batteryCharging.value = battery.charging;
+      batteryLevel.value = battery.level;
+      battery.addEventListener('levelchange', (e) => {
+        batteryLevel.value = e.currentTarget.level;
+      });
+      battery.addEventListener('chargingchange', (e) => {
+        batteryCharging.value = e.currentTarget.charging
+      });
+    });
+  }
+
+  getBatteryStatus();
 </script>
 
 <style scoped>
