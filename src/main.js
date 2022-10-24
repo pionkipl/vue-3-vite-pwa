2fcfda52086a0132baa94613cef590e35a445004
Jarek <j.szczepaniak@albedomarketing.pl>
2022-10-24 22:33:56 +0200

    Feat: add animate +. add text animatet.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import './animate/animate.js'
+
 import './assets/main.css'
 
 import { registerSW } from 'virtual:pwa-register'
@@ -15,52 +17,53 @@ const updateSW = registerSW({
   },
 })
 
-import { initializeApp } from "firebase/app";
-import { getMessaging, getToken, onMessage } from "firebase/messaging";
+// import { initializeApp } from "firebase/app";
+// import { getMessaging, getToken, onMessage } from "firebase/messaging";
+//
+//
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_APP_KEY,
+//   authDomain: "vue-3-pwa.firebaseapp.com",
+//   projectId: "vue-3-pwa",
+//   storageBucket: "vue-3-pwa.appspot.com",
+//   messagingSenderId: import.meta.env.VITE_APP_SENDER_ID,
+//   appId: import.meta.env.VITE_APP_ID,
+//   measurementId: "G-KSCPZM1BB2"
+// };
+//
+// const appFb = initializeApp(firebaseConfig);
+//
+// const messaging = getMessaging(appFb);
+//
+// const vapidKey = import.meta.env.VITE_APP_VAPID_KEY;
+//
+// function requestPermission() {
+//   console.log('Requesting permission...');
+//   Notification.requestPermission().then((permission) => {
+//     if (permission === 'granted') {
+//       console.log('Notification permission granted.');
+//
+//       getToken(messaging,{ vapidKey: vapidKey }).then((currentToken) => {
+//         if (currentToken) {
+//           console.log('token', currentToken)
+//           onMessage(messaging, (payload) => {
+//             console.log('Message received. ', payload);
+//             // ...
+//           });
+//         } else {
+//           // Show permission request UI
+//           console.log('No registration token available. Request permission to generate one.');
+//         }
+//       }).catch((err) => {
+//         console.log('An error occurred while retrieving token. ', err);
+//         // ...
+//       });
+//     }
+//   })
+// }
+//
+// requestPermission();
 
-
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APP_KEY,
-  authDomain: "vue-3-pwa.firebaseapp.com",
-  projectId: "vue-3-pwa",
-  storageBucket: "vue-3-pwa.appspot.com",
-  messagingSenderId: import.meta.env.VITE_APP_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
-  measurementId: "G-KSCPZM1BB2"
-};
-
-const appFb = initializeApp(firebaseConfig);
-
-const messaging = getMessaging(appFb);
-
-const vapidKey = import.meta.env.VITE_APP_VAPID_KEY;
-
-function requestPermission() {
-  console.log('Requesting permission...');
-  Notification.requestPermission().then((permission) => {
-    if (permission === 'granted') {
-      console.log('Notification permission granted.');
-
-      getToken(messaging,{ vapidKey: vapidKey }).then((currentToken) => {
-        if (currentToken) {
-          console.log('token', currentToken)
-          onMessage(messaging, (payload) => {
-            console.log('Message received. ', payload);
-            // ...
-          });
-        } else {
-          // Show permission request UI
-          console.log('No registration token available. Request permission to generate one.');
-        }
-      }).catch((err) => {
-        console.log('An error occurred while retrieving token. ', err);
-        // ...
-      });
-    }
-  })
-}
-
-requestPermission();
 
 
 const app = createApp(App)
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,71 @@
-<script setup>
-
-</script>
-
 <template>
   <main>
-    <h1>Brand new app</h1>
-    <p>Add another text.</p>
+    <h1 class="heading-text text-center text-2xl md:text-5xl">
+      <span class="letter">P</span>
+      <span class="letter">W</span>
+      <span class="letter">A</span>
+      <span class="letter">&nbsp;</span>
+      <span class="letter">G</span>
+      <span class="letter">E</span>
+      <span class="letter">T</span>
+      <span class="letter">&nbsp;</span>
+      <span class="letter">S</span>
+      <span class="letter">T</span>
+      <span class="letter">A</span>
+      <span class="letter">R</span>
+      <span class="letter">T</span>
+      <span class="letter">E</span>
+      <span class="letter">D</span>
+    </h1>
   </main>
 </template>
 
+<script setup>
+  import { onMounted } from 'vue';
+  import anime from 'animejs';
+
+  onMounted(() => {
+    anime.timeline({
+      loop: false,
+      targets: '.heading-text .letter',
+    })
+        .add({
+          targets: '.heading-text .letter',
+          scale: [4,1],
+          opacity: [0,1],
+          translateX: [-150, 0],
+          easing: "easeOutExpo",
+          duration: 700,
+          delay: (el, i) => 200*i
+          })
+  })
+
+</script>
 <style lang="scss" scoped>
 main {
   margin-top: 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: calc(100% - 4rem);
 
   @media screen and (max-width: 768px) {
     padding: 0 15px;
+  }
+
+  .heading-text {
+    font-weight: bold;
+    margin-bottom: 8rem;
+    color: #fff;
+    display: block;
+
+    .letter {
+      display: inline-block;
+      background: linear-gradient(180deg, rgba(94,234,212,1) 0%, rgba(253,164,175,1) 62%, rgba(252,211,77,1) 100%);
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: transparent;
+      font-weight: bold;
+    }
   }
 }
 </style>
