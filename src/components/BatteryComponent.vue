a5459517796449b4e90e02d98423a4ef6d13b566
Jarek <j.szczepaniak@albedomarketing.pl>
2022-10-26 13:13:46 +0200

    Feat: add new device fetaures including battery component.

---
+++ src/components/BatteryComponent.vue
@@ -0,0 +1,284 @@
+<template>
+  <div class="g-container">
+    <div class="g-number">{{ Math.round(batteryLevel * 100) }}%</div>
+    <div class="g-contrast">
+      <div class="g-circle"></div>
+      <ul class="g-bubbles">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  import { ref } from 'vue';
+
+  const props = defineProps({
+    battery: Number
+  })
+  const batteryLevel = ref(props.battery)
+</script>
+
+<style scoped lang="scss">
+.g-container {
+  width: 300px;
+  height: 500px;
+
+  @media screen and (max-width: 600px) {
+    width: 200px;
+  }
+}
+
+.g-number {
+  position: absolute;
+  width: 300px;
+  top: 18%;
+  text-align: center;
+  font-size: 40px;
+  z-index: 10;
+  color: #fff;
+
+  @media screen and (max-width: 600px) {
+    width: 200px;
+  }
+}
+
+.g-contrast {
+  filter: contrast(10) hue-rotate(0);
+  width: 100%;
+  height: 100%;
+  /*background-color: #2c3e50;*/
+  overflow: hidden;
+  animation: hueRotate 10s infinite linear;
+
+  @media screen and (max-width: 600px) {
+    width: 200px;
+  }
+
+}
+
+.g-circle {
+  position: relative;
+  width: 300px;
+  height: 300px;
+  box-sizing: border-box;
+
+  @media screen and (max-width: 600px) {
+    width: 200px;
+  }
+  /*filter: blur(8px);*/
+}
+
+.g-circle::after {
+  content: "";
+  position: absolute;
+  top: 40%;
+  left: 50%;
+  transform: translate(-50%, -50%) rotate(0);
+  width: 200px;
+  height: 200px;
+  background-color: #00ff6f;
+  border-radius: 42% 38% 62% 49% / 45%;
+  animation: rotate 10s infinite linear;
+}
+.g-circle::before {
+  content: "";
+  position: absolute;
+  width: 176px;
+  height: 176px;
+  top: 40%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border-radius: 50%;
+  background-color: #2c3e50;
+  z-index: 10;
+}
+
+.g-bubbles {
+  position: absolute;
+  left: 50%;
+  bottom: 0;
+  width: 100px;
+  height: 40px;
+  transform: translate(-50%, 0);
+  border-radius: 100px 100px 0 0;
+  background-color: #00ff6f;
+  /*filter: blur(5px);*/
+}
+
+li {
+  position: absolute;
+  border-radius: 50%;
+  background: #00ff6f;
+  list-style: none;
+}
+
+li:nth-of-type(1) {
+  left: 66px;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 24px;
+  height: 24px;
+  animation: moveToTop 6s ease-in-out -3.779s infinite;
+}
+li:nth-of-type(2) {
+  left: 76px;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 28px;
+  height: 28px;
+  animation: moveToTop 7s ease-in-out -2.015s infinite;
+}
+li:nth-of-type(3) {
+  left: 30px;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 22px;
+  height: 22px;
+  animation: moveToTop 7s ease-in-out -2.362s infinite;
+}
+li:nth-of-type(4) {
+  left: 40px;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 19px;
+  height: 19px;
+  animation: moveToTop 6s ease-in-out -1.994s infinite;
+}
+li:nth-of-type(5) {
+  left: 47px;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 18px;
+  height: 18px;
+  animation: moveToTop 5s ease-in-out -1.175s infinite;
+}
+li:nth-of-type(6) {
+  left: 37px;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 23px;
+  height: 23px;
+  animation: moveToTop 9s ease-in-out -0.056s infinite;
+}
+li:nth-of-type(7) {
+  left: 49px;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 24px;
+  height: 24px;
+  animation: moveToTop 4s ease-in-out -0.742s infinite;
+}
+li:nth-of-type(8) {
+  left: 83px;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 27px;
+  height: 27px;
+  animation: moveToTop 4s ease-in-out -0.956s infinite;
+}
+li:nth-of-type(9) {
+  left: 16px;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 23px;
+  height: 23px;
+  animation: moveToTop 6s ease-in-out -0.574s infinite;
+}
+li:nth-of-type(10) {
+  left: 79px;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 17px;
+  height: 17px;
+  animation: moveToTop 6s ease-in-out -2.908s infinite;
+}
+li:nth-of-type(11) {
+  left: 37px;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 20px;
+  height: 20px;
+  animation: moveToTop 6s ease-in-out -3.269s infinite;
+}
+li:nth-of-type(12) {
+  left: 64px;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 19px;
+  height: 19px;
+  animation: moveToTop 8s ease-in-out -0.052s infinite;
+}
+li:nth-of-type(13) {
+  left: 79px;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 29px;
+  height: 29px;
+  animation: moveToTop 8s ease-in-out -0.011s infinite;
+}
+li:nth-of-type(14) {
+  left: 75px;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 28px;
+  height: 28px;
+  animation: moveToTop 7s ease-in-out -4.447s infinite;
+}
+li:nth-of-type(15) {
+  left: 63px;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 25px;
+  height: 25px;
+  animation: moveToTop 4s ease-in-out -1.356s infinite;
+}
+li:nth-of-type(16) {
+  left: 36px;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 16px;
+  height: 16px;
+  animation: moveToTop 6s ease-in-out -0.667s infinite;
+}
+
+@keyframes rotate {
+  50% {
+    border-radius: 45% / 42% 38% 58% 49%;
+  }
+  100% {
+    transform: translate(-50%, -50%) rotate(720deg);
+  }
+}
+
+@keyframes moveToTop {
+  90% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0.1;
+    transform: translate(-50%, -280px);
+  }
+}
+
+@keyframes hueRotate {
+  100% {
+    filter: contrast(15) hue-rotate(360deg);
+  }
+}
+
+</style>
--- src/views/DeviceFeatures.vue
+++ src/views/DeviceFeatures.vue
@@ -1,14 +1,20 @@
 <template>
-  <div class="device-feature">
-    <h2>Battery</h2>
-    <p>Status: {{ batteryCharging ? 'Charging' : 'Not charging' }}</p>
-    <p style="margin-bottom: 1rem">Level: {{ batteryLevel * 100 }}%</p>
-    <button @click="vibrate" class="button">Vibrate</button>
+  <div class="device-feature" :class="{pad: !batteryCharging}">
+    <h1 class="text-xl md:text-3xl mb-5">Device features</h1>
+    <button @click="vibrate" class="button bg-teal-400 text-black">Vibrate</button>
+    <h2 class="text-xl md:text-3xl mb-2">Battery</h2>
+    <p class="text-xl md:text-3xl mb-2">Status: <span :class="[{'text-lime-400': batteryCharging}, {'text-pink-400': !batteryCharging}]">{{ batteryCharging ? 'Charging' : 'Not charging' }}</span></p>
+    <p v-if="!batteryCharging" class="text-xl md:text-3xl mb-2">Battery Left: <span class="text-teal-400">{{ batteryDischargingTime !== null ? batteryDischargingTime / 60 : 'Estimating...' }} minutes left</span></p>
+    <p v-if="batteryCharging" class="text-xl md:text-3xl mb-2">Battery to fully charged: <span class="text-teal-400">{{ batteryChargingTime !== null ? batteryChargingTime / 60 : 'Estimating...' }} minutes</span></p>
+
+    <p v-if="!batteryCharging" class="text-xl md:text-2xl font-bold">Level: {{ batteryLevel * 100 }}%</p>
+    <battery-component :battery="batteryLevel" v-else />
   </div>
 </template>
 
 <script setup>
   import { ref } from 'vue';
+  import BatteryComponent from "../components/BatteryComponent.vue";
 
   function vibrate() {
     navigator.vibrate([100, 200, 100, 200, 100, 200, 100, 200, 100,
@@ -19,6 +25,8 @@
 
   let batteryCharging = ref(null);
   let batteryLevel = ref(null);
+  let batteryChargingTime = ref(null);
+  let batteryDischargingTime = ref(null);
 
   let batteryPromise;
 
@@ -34,9 +42,18 @@
       batteryLevel.value = battery.level;
       battery.addEventListener('levelchange', (e) => {
         batteryLevel.value = e.currentTarget.level;
+
       });
       battery.addEventListener('chargingchange', (e) => {
         batteryCharging.value = e.currentTarget.charging
+      });
+      battery.addEventListener('chargingtimechange', (e) => {
+        batteryChargingTime.value = e.currentTarget.chargingTime
+        console.log('chargingtime', e)
+      });
+      battery.addEventListener('dischargingtimechange', (e) => {
+        batteryDischargingTime.value = e.currentTarget.dischargingTime
+        console.log('discharging', e)
       });
     });
   }
@@ -45,9 +62,13 @@
 </script>
 
 <style scoped>
-  .device-feature {  background-color: #2c3e50;
-    padding: 2rem;
+  .device-feature {
+    background-color: #2c3e50;
+    padding: 2rem 2rem 0 2rem;
     margin-top: 1rem;
+  }
 
+  .pad {
+    padding: 2rem;
   }
 </style>
