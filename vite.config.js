c1742bad0588912987d7ea2f5974b78747194e66
Jarek <j.szczepaniak@albedomarketing.pl>
2022-10-27 12:03:51 +0200

    Feat: add new vite config manifest/

--- vite.config.js
+++ vite.config.js
@@ -22,12 +22,23 @@ export default defineConfig({
         workbox: {
           globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
         },
-        includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg', 'pwa-192x192.png', 'pwa-512x512.png'],
+        includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg', 'pwa-192x192.png', 'pwa-512x512.png', 'can.png'],
         manifest: {
           name: 'My Awesome App',
           short_name: 'My vite pwa',
           description: 'My Awesome App vite pwa',
-          theme_color: '#ffffff',
+          theme_color: '#36c',
+          display_override: ["window-controls-overlay"],
+          shortcuts: [
+            {
+              "name": "Mysterious",
+              "short_name": "Mysterious",
+              "url": "/mysterious",
+              "icons": [
+                { "src": "/can.png", "sizes": "300x520", "type": "image/png" }
+              ]
+            },
+          ],
           icons: [
             {
               src: 'pwa-192x192.png',
